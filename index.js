1c267fdff18b1ca5fdbf6e5397dbf620a63cb5ae
Menno van Balen <mennovanbalen@gmail.com>
2022-11-28 18:59:14 +0000

    Implemented unique keys for translation json

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ import make from "./lib/make.js";
 
 const args = argv.slice(2);
 
-const arr = args[1]?.split(".") || null;
-const ext = arr === null ? null : arr[arr.length - 1];
-const arr2 = args[2]?.split(".") || null;
-const extTmx = arr2 === null ? undefined : arr2[arr2.length - 1];
+const arg1 = args[1]?.split(".") || null;
+const ext = arg1 === null ? null : arg1[arg1.length - 1];
+const arg2 = args[2]?.split(".") || null;
+const extTmx = arg2 === null ? undefined : arg2[arg2.length - 1];
 
 const commandCondition = args[0] !== "extract" && args[0] !== "make";
-const lengthCondition =
-  (args[0] === "extract" && args.length !== 2) ||
-  (args[0] === "make" && args.length !== 3);
+const lengthCondition = args.length !== 3;
+// (args[0] === "extract" && args.length !== 3) ||
+// (args[0] === "make" && args.length !== 3);
+
 const fileConditionExtract =
-  args[0] === "extract" && ext !== "mdx" && ext !== "md";
+  args[0] === "extract" && ext !== "mdx" && ext !== "md" && args[2];
 const fileConditionMake =
   (args[0] === "make" && ext !== "json") ||
   (args[0] === "make" && extTmx !== "json");
@@ -28,20 +29,20 @@ if (
   if (commandCondition) console.log("\nHint: not the right command");
   if (lengthCondition) console.log("\nHint: not the right ammount of inputs");
   if (fileConditionExtract)
-    console.log("\nHint: not the right input file extension (.mdx)");
+    console.log("\nHint: not the right input file extension (.mdx or .md)");
   if (fileConditionMake)
     console.log(
       "\nHint: not the right input file or mdx-tree file extension (.json)"
     );
   console.log(
-    "\nUsage: extract input.mdx\n   or: make translated.json mdx-tree.json\n"
+    "\nUsage: extract input.mdx keyId\n   or: make translated.json mdx-tree.json\n"
   );
   exit(1);
 } else if (args[0] === "extract") {
-  extract(args[1]);
-  console.log(`File extracted to: ${arr[0]}.json`);
+  extract(args[1], args[2]);
+  console.log(`File extracted to: ${arg1[0]}.json`);
 } else {
   make(args[1], args[2]);
-  console.log(`New mdx file created: ${arr2[0]}-translated.mdx`);
+  console.log(`New mdx file created: ${arg2[0]}-translated.mdx`);
 }
 exit(0);
